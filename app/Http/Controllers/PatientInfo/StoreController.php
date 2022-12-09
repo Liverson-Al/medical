822a99ab39d4efa03a80d4a04c93e1ffe33c9fe1
Liverson-Al <80148366+Liverson-Al@users.noreply.github.com>
2022-12-09 18:27:00 +0300

    Сделал создание по всем таблицам одновременно, но id не соотносятся,

--- app/Http/Controllers/PatientInfo/StoreController.php
+++ app/Http/Controllers/PatientInfo/StoreController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\PatientInfo;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PatientInfo\StoreRequest;
-use App\Http\Resources\PatientInfo\PatientInfoResource;
 use App\Models\Anthropometry;
 use App\Models\ClinicalData;
 use App\Models\ConcomDisease;
@@ -23,45 +22,59 @@ class StoreController extends BaseController
         $employee_id = $request->employee_id;
 
         $personal_data = new PatientInfo();
+        $personal_data->id = $patientID;
         $personal_data->fill($request->personal_data);
         $personal_data->saveOrFail();
 
-        $anthropometric_data = new Anthropometry();
-        $anthropometric_data->patient_id = $patientID;
-        $anthropometric_data->user_id = $employee_id;
-        $anthropometric_data->fill($request->anthropometric_data);
-        $anthropometric_data->saveOrFail();
+        if ($request->anthropometric_data) {
+            $anthropometric_data = new Anthropometry();
+            $anthropometric_data->patient_id = $patientID;
+            $anthropometric_data->user_id = $employee_id;
+            $anthropometric_data->fill($request->anthropometric_data);
+            $anthropometric_data->saveOrFail();
+        }
 
-        $clinical_data = new ClinicalData();
-        $clinical_data->visit_id = $patientID;
-        $clinical_data->user_id = $employee_id;
-        $clinical_data->fill($request->clinical_data);
-        $clinical_data->saveOrFail();
+        if ($request->clinical_data) {
+            $clinical_data = new ClinicalData();
+            $clinical_data->visit_id = $patientID;
+            $clinical_data->user_id = $employee_id;
+            $clinical_data->fill($request->clinical_data);
+            $clinical_data->saveOrFail();
+        }
 
-        $concom_desease = new ConcomDisease();
-        $concom_desease->patient_id = $patientID;
-        $concom_desease->user_id = $employee_id;
-        $concom_desease->fill($request->concom_desease);
-        $concom_desease->saveOrFail();
+        if($request->concom_desease) {
+            $concom_desease = new ConcomDisease();
+            $concom_desease->patient_id = $patientID;
+            $concom_desease->user_id = $employee_id;
+            $concom_desease->fill($request->concom_desease);
+            $concom_desease->saveOrFail();
+        }
 
 
-        $anamnesis = new PatientHistory();
-        $anamnesis->patient_id = $patientID;
-        $anamnesis->user_id = $employee_id;
-        $anamnesis->fill($request->anamnesis);
-        $anamnesis->saveOrFail();
+        if ($request->anamnesis) {
+            $anamnesis = new PatientHistory();
+            $anamnesis->patient_id = $patientID;
+            $anamnesis->user_id = $employee_id;
+            $anamnesis->fill($request->anamnesis);
+            $anamnesis->saveOrFail();
+        }
 
-        $echocardiogram = new Echocardiography();
-        $echocardiogram->visit_id = $patientID;
-        $echocardiogram->user_id = $employee_id;
-        $echocardiogram->fill($request->echocardiogram);
-        $echocardiogram->saveOrFail();
+        if ($request->echocardiogram) {
+            $echocardiogram = new Echocardiography();
+            $echocardiogram->visit_id = $patientID;
+            $echocardiogram->user_id = $employee_id;
+            $echocardiogram->fill($request->echocardiogram);
+            $echocardiogram->saveOrFail();
+        }
 
-        $MCT = new MSQCAngiographyAorta();
-        $echocardiogram->VisitPatientID = $patientID;
-        $echocardiogram->user_id = $employee_id;
-        $MCT->fill($request->MCT);
-        $MCT->saveOrFail();
+        if ($request->MCT) {
+            $MCT = new MSQCAngiographyAorta();
+            $echocardiogram->VisitPatientID = $patientID;
+            $echocardiogram->user_id = $employee_id;
+            $MCT->fill($request->MCT);
+            $MCT->saveOrFail();
+        }
+
 
 //        $patient = $request->validated();
 //        $patient = Patientinfo::create($patient);
